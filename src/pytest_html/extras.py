f8387e172caf5680a2ff9d920b364b2d990bc7ab
anatomst <www.shpachyk98@gmail.com>
2023-04-20 18:07:35 +0300

    added enums

--- src/pytest_html/extras.py
+++ src/pytest_html/extras.py
@@ -1,19 +1,22 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-FORMAT_HTML = "html"
-FORMAT_IMAGE = "image"
-FORMAT_JSON = "json"
-FORMAT_TEXT = "text"
-FORMAT_URL = "url"
-FORMAT_VIDEO = "video"
+from enum import Enum
 
 
-def extra(content, format_type, name=None, mime_type=None, extension=None):
+class Format(Enum):
+    HTML = "html"
+    IMAGE = "image"
+    JSON = "json"
+    TEXT = "text"
+    URL = "url"
+    VIDEO = "video"
+
+
+def extra(content, format_type: Format, name=None, mime_type=None, extension=None):
     return {
         "name": name,
-        "format_type": format_type,
+        "format_type": format_type.value,
         "content": content,
         "mime_type": mime_type,
         "extension": extension,
@@ -21,11 +24,11 @@ def extra(content, format_type, name=None, mime_type=None, extension=None):
 
 
 def html(content):
-    return extra(content, FORMAT_HTML)
+    return extra(content, Format.HTML)
 
 
 def image(content, name="Image", mime_type="image/png", extension="png"):
-    return extra(content, FORMAT_IMAGE, name, mime_type, extension)
+    return extra(content, Format.IMAGE, name, mime_type, extension)
 
 
 def png(content, name="Image"):
@@ -41,19 +44,19 @@ def svg(content, name="Image"):
 
 
 def json(content, name="JSON"):
-    return extra(content, FORMAT_JSON, name, "application/json", "json")
+    return extra(content, Format.JSON, name, "application/json", "json")
 
 
 def text(content, name="Text"):
-    return extra(content, FORMAT_TEXT, name, "text/plain", "txt")
+    return extra(content, Format.TEXT, name, "text/plain", "txt")
 
 
 def url(content, name="URL"):
-    return extra(content, FORMAT_URL, name)
+    return extra(content, Format.URL, name)
 
 
 def video(content, name="Video", mime_type="video/mp4", extension="mp4"):
-    return extra(content, FORMAT_VIDEO, name, mime_type, extension)
+    return extra(content, Format.VIDEO, name, mime_type, extension)
 
 
 def mp4(content, name="Video"):
