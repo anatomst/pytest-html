c6f8e17fc5a6816019e0d5bfa1c4b931f3e65852
anatomst <www.shpachyk98@gmail.com>
2023-04-20 18:11:15 +0300

    added try/except construction

--- src/pytest_html/util.py
+++ src/pytest_html/util.py
@@ -1,17 +1,29 @@
 import importlib
 import json
 from functools import lru_cache
+from functools import partial
 from typing import Any
 from typing import Dict
+
+from _pytest.logging import _remove_ansi_escape_sequences
+from ansi2html import Ansi2HTMLConverter
+from ansi2html import style
+
+
+try:
+    converter = Ansi2HTMLConverter(inline=False, escaped=False)
+    _handle_ansi = partial(converter.convert, full=False)
+    _ansi_styles = style.get_styles()
+except ImportError:
+    _handle_ansi = _remove_ansi_escape_sequences
+    _ansi_styles = []
 
 
 @lru_cache()
 def ansi_support():
     try:
-        # from ansi2html import Ansi2HTMLConverter, style  # NOQA
         return importlib.import_module("ansi2html")
     except ImportError:
-        # ansi2html is not installed
         pass
 
 
