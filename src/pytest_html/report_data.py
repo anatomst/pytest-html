8922f8e757f4961c46d3e2971c87fa479a080852
anatomst <www.shpachyk98@gmail.com>
2023-04-21 10:43:30 +0300

    added docstrings

--- src/pytest_html/report_data.py
+++ src/pytest_html/report_data.py
@@ -5,6 +5,10 @@ from pytest_html.util import _handle_ansi
 
 
 class ReportData:
+    """
+    A class for storing report data in dictionary format.
+    """
+
     def __init__(self, title, config):
         self._config = config
         self._data = {
@@ -31,25 +35,40 @@ class ReportData:
             )
 
     @property
-    def title(self):
+    def title(self) -> str:
+        """
+        Returns the current title of the report.
+        """
         return self._data["title"]
 
     @title.setter
-    def title(self, title):
+    def title(self, title: str) -> None:
+        """
+        Sets the title of the report to a new string.
+        """
         self._data["title"] = title
 
     @property
     def config(self):
+        """
+        Returns the current pytest configuration object.
+        """
         return self._config
 
     @property
-    def data(self):
+    def data(self) -> dict:
+        """
+        Returns the current report data dictionary.
+        """
         return self._data
 
-    def set_data(self, key, value):
+    def set_data(self, key, value) -> None:
         self._data[key] = value
 
-    def add_test(self, test_data, report, row, remove_log=False):
+    def add_test(self, test_data, report, row, remove_log=False) -> bool:
+        """
+        Adds the information for a test to the report data dictionary.
+        """
         for sortable, value in row.sortables.items():
             test_data[sortable] = value
 
@@ -69,7 +88,10 @@ class ReportData:
 
         return False
 
-    def update_test_log(self, report):
+    def update_test_log(self, report) -> None:
+        """
+        Adds the log for a test to the report data dictionary.
+        """
         log = []
         for test in self._data["tests"][report.nodeid]:
             if test["testId"] == report.nodeid and "log" in test:
@@ -81,7 +103,10 @@ class ReportData:
                 test["log"] += _handle_ansi("\n".join(log))
 
 
-def _process_logs(report):
+def _process_logs(report) -> str:
+    """
+    Process the logs for a pytest report.
+    """
     log = []
     if report.longreprtext:
         log.append(report.longreprtext.replace("<", "&lt;").replace(">", "&gt;") + "\n")
